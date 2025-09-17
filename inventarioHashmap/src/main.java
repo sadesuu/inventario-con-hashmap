4f43f86c0f79df158d89d24db373607af221a40a
Hugo Solís Torrijos <133656458+sadesuu@users.noreply.github.com>
2025-09-17 19:50:54 +0200

    Add files via upload

---
+++ inventarioHashmap/src/main.java
@@ -0,0 +1,97 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        HashMap<String, producto> mapa = new HashMap<>();
+        inventario inv = new inventario(mapa);
+        Scanner sc = new Scanner(System.in);
+        boolean bucle = true;
+        while (bucle) {
+            System.out.println("Bienvenido al sistema de inventario");
+            System.out.println("Seleccione una opcion:");
+            System.out.println("1. Añadir producto");
+            System.out.println("2. Actualizar stock");
+            System.out.println("3. Vender producto");
+            System.out.println("4. Buscar producto");
+            System.out.println("5. Filtrar por categoria");
+            System.out.println("6. Tamaño del inventario");
+            System.out.println("7. Precio total del inventario");
+            System.out.println("8. Mostrar inventario");
+            System.out.println("9. Salir");
+
+            String opcion = sc.nextLine();
+
+
+
+
+            switch (opcion) {
+                case "1":
+                    System.out.println("Ingrese el codigo del producto:");
+                    String codigo = sc.nextLine();
+                    System.out.println("Ingrese el nombre del producto:");
+                    String nombre = sc.nextLine();
+                    System.out.println("Ingrese el precio del producto:");
+                    double precio = Double.parseDouble(sc.nextLine());
+                    System.out.println("Ingrese la categoria del producto (Laptops, Smartphones, Tablets, Smartwatch, SmartTV):");
+                    String categoriaStr = sc.nextLine();
+                    categoria.categorias categoriaEnum = categoria.categorias.valueOf(categoriaStr);
+                    System.out.println("Ingrese el stock del producto:");
+                    int stock = Integer.parseInt(sc.nextLine());
+
+                    producto p = new producto(codigo, nombre, precio, categoriaEnum, stock);
+                    inv.añadirProducto(p);
+                    break;
+                case "2":
+                    System.out.println("Ingrese el nombre del producto a actualizar:");
+                    String nombreActualizar = sc.nextLine();
+                    System.out.println("Ingrese el nuevo stock:");
+                    int nuevoStock = Integer.parseInt(sc.nextLine());
+                    inv.actualizarStock(nombreActualizar, nuevoStock);
+                    break;
+                case "3":
+                    System.out.println("Ingrese el nombre del producto a vender:");
+                    String nombreVender = sc.nextLine();
+                    System.out.println("Ingrese la cantidad a vender:");
+                    int cantidad = Integer.parseInt(sc.nextLine());
+                    inv.venderproducto(nombreVender, cantidad);
+                    break;
+                case "4":
+                    System.out.println("Ingrese el nombre del producto a buscar:");
+                    String nombreBuscar = sc.nextLine();
+                    inv.buscarProducto(nombreBuscar);
+                    break;
+                case "5":
+                    System.out.println("Ingrese la categoria a filtrar (Laptops, Smartphones, Tablets, Smartwatch, SmartTV):");
+                    String categoriaFiltrarStr = sc.nextLine();
+                    categoria.categorias categoriaFiltrarEnum = categoria.categorias.valueOf(categoriaFiltrarStr);
+                    inv.filtrarPorCategoria(categoriaFiltrarEnum);
+                    break;
+                case "6":
+                    inv.tamanoInventarioPorStock();
+                    break;
+                case "7":
+                    inv.precioTotal();
+                    break;
+
+                case "8":
+                    inv.mostrarInventario();
+                    break;
+                case "9":
+                    bucle = false;
+                    System.out.println("Saliendo del sistema de inventario...");
+                    break;
+
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+
+        }
+
+
+
+
+        sc.close();
+    }
+}
---
+++ inventarioHashmap/src/producto.java
@@ -0,0 +1,52 @@
+public class producto {
+
+    private String codigo;
+    private String nombre;
+    private double precio;
+    private categoria.categorias categoria;
+    private int stock;
+
+
+    public producto(String codigo, String nombre, double precio, categoria.categorias categoria, int stock) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+        this.stock = stock;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public categoria.categorias getCategoria() {
+        return categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "codigo='" + codigo + "', nombre='" + nombre + "', precio=" + precio +
+               ", categoria=" + categoria + ", stock=" + stock;
+    }
+}
